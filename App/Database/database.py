1fa6ac01383ce8357a908a5441a7718d677a9304
Eduardo <programador19@tecnologicocomfenalco.edu.co>
2025-11-04 14:24:17 -0500

    feat: actualizacion en la base de datos

--- App/Database/database.py
+++ App/Database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from App.Core.config import settings
 
-DATABASE_URL = f"postgresql://leviatan_user:DUHx0MorTz4LzTtauZNYeGlzlOs8xl4Y@dpg-d3uk8mje5dus739oocj0-a/leviatan"
+DATABASE_URL = f"postgresql://leviatan_v2_user:isExx5RXtuRkJLjcAkZyRMGrF5KdvlmJ@dpg-d45540s9c44c73816nu0-a/leviatan_v2"
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
